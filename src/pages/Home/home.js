325c67eec993aeaef5c06c21612698ec3ebdc40d
Minha Kwon <namisoft@naver.com>
2022-06-19 02:31:51 +0900

    tradingview card style 변경

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -93,42 +93,46 @@ const Home = () => {
   return (
     <div className="container">
       <div className="col">
-        <nav className={"navbar navbar-expand mb-2"}>
-          <div className="container d-flex flex-row">
-            <ul className="nav navbar-nav text-dark">
-              <li
-                className={`${styles.navItem} mr-2`}
-                role="presentation"
-                onClick={() => {
-                  onClickList("KOSPI");
-                }}
-              >
-                <span className="text-muted">코스피</span>
-              </li>
-              <span className="text-muted">|</span>
-              <li
-                className={`${styles.navItem} ml-2 mr-2`}
-                role="presentation"
-                onClick={() => {
-                  onClickList("KOSDAQ");
-                }}
-              >
-                <span className="text-muted">코스닥</span>
-              </li>
-              <span className="text-muted">|</span>
-              <li
-                className={`${styles.navItem} ml-2`}
-                role="presentation"
-                onClick={() => {
-                  onClickList("FUT");
-                }}
-              >
-                <span className="text-muted">선물</span>
-              </li>
-            </ul>
+        <div className="card shadow py-2">
+          <div className="card-body">
+            <nav className={"navbar navbar-expand mb-2"}>
+              <div className="container d-flex flex-row">
+                <ul className="nav navbar-nav text-dark">
+                  <li
+                    className={`${styles.navItem} mr-2`}
+                    role="presentation"
+                    onClick={() => {
+                      onClickList("KOSPI");
+                    }}
+                  >
+                    <span className="text-muted">코스피</span>
+                  </li>
+                  <span className="text-muted">|</span>
+                  <li
+                    className={`${styles.navItem} ml-2 mr-2`}
+                    role="presentation"
+                    onClick={() => {
+                      onClickList("KOSDAQ");
+                    }}
+                  >
+                    <span className="text-muted">코스닥</span>
+                  </li>
+                  <span className="text-muted">|</span>
+                  <li
+                    className={`${styles.navItem} ml-2`}
+                    role="presentation"
+                    onClick={() => {
+                      onClickList("FUT");
+                    }}
+                  >
+                    <span className="text-muted">선물</span>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <div id="tvChart" ref={tvChartRef}></div>
           </div>
-        </nav>
-        <div id="tvChart" ref={tvChartRef}></div>
+        </div>
       </div>
     </div>
   );
